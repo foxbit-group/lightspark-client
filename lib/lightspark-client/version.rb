9a617940a5908e1733df4b98243c37e4f6acecc1
Cruwal <wallace.souza@foxbit.com.br>
2024-10-25 14:24:09 -0300

    remove PR template

--- lib/lightspark-client/version.rb
+++ lib/lightspark-client/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module LightsparkClient
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
--- spec/lightspark_client/version_spec.rb
+++ spec/lightspark_client/version_spec.rb
@@ -4,6 +4,6 @@ require "spec_helper"
 
 RSpec.describe LightsparkClient do
   it "expects to have the correct version" do
-    expect(LightsparkClient::VERSION).to be "0.1.1"
+    expect(LightsparkClient::VERSION).to be "0.1.2"
   end
 end
